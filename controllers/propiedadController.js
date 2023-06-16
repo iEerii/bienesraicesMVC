d372c7a38d9553474d9a49cf7ec7dc80d4a5ae5b
iEerii <erickaponce92@gmail.com>
2023-06-16 12:41:48 -0500

    Creando el formulario para propiedades

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -2,10 +2,20 @@
 
 const admin = (request, response) => {
     response.render('propiedades/admin', {
-        pagina: 'Mis propiedades'
+        pagina: 'Mis propiedades',
+        barra: true
+    })
+}
+
+//formulario para crear una nueva propiedad
+const crear = (request, response) => {
+    response.render('propiedades/crear', {
+        pagina: 'Crear propiedad',
+        barra: true
     })
 }
 
 export {
-    admin
+    admin,
+    crear
 }
