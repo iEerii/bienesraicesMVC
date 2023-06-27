613849c928cb8ce26b145e7cf68fb74dda90e788
iEerii <erickaponce92@gmail.com>
2023-06-27 12:23:34 -0500

    Mostrar el mapa y obtener latitud, logintud e informacion de una calle o ciudad

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,7 +3,10 @@
     const lat = 34.0417856;
     const lng = -118.2623532;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
-    
+    let marker
+
+    //Utilizar Provider y Geocode
+    const geocodeService = L.esri.Geocoding.geocodeService();
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -17,12 +20,15 @@
     .addTo(mapa)
 
     //Detectar el movimiento del pin 
-    marker.on('moveend', function(){
+    marker.on('moveend', function(e){
         marker = e.target
-        
         const posicion= marker.getLatLgn()
+        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
 
-        mapa.panTo(new L.LatLng.lat, posicion.lng)
+        //Obtener la infomación de las calles al soltar el pin
+        geocodeService.reverse().latlng(posicion, 13).run(function(error, resultado) {
+            marker.bindPopup(resultado.address.LongLabel)
+        })
     })
 
 })()
